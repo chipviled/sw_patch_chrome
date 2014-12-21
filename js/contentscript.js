2cb4c79432e89e8dd13892482f0176dc5dc83c4b
Chip Viled <chipviled@gmail.com>
2014-12-21 17:53:57 +0200

    Add loading.gif in base 64 for shadowbox.

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,27 +1,25 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('js/jquery-1.11.1.min.js');
-(document.head||document.documentElement).appendChild(s);
-s.onload = function() {
-    s.parentNode.removeChild(s);
 
+loadJavaScript('js/jquery-1.11.1.min.js', function() {
     // Get config from background.js
     chrome.runtime.sendMessage('getOptions', function(response){
-                    options = response ? response : {};
-
         var s2 = document.createElement('script');
+        options = response ? response : {};
         s2.text = 'var sw_config = ' + JSON.stringify(options) + ';';
         (document.head||document.documentElement).appendChild(s2);
-        s2.onload = function() {
-            s2.parentNode.removeChild(s2);
-        };
+        loadJavaScript('js/common.js');
     });
+});
 
-    var s3 = document.createElement('script');
-    s3.src = chrome.extension.getURL('js/common.js');
-    (document.head||document.documentElement).appendChild(s3);
-    s3.onload = function() {
-        s3.parentNode.removeChild(s3);
+
+function loadJavaScript(js, callback) {
+    var sv = document.createElement('script');
+    sv.src = chrome.extension.getURL(js);
+    (document.head||document.documentElement).appendChild(sv);
+    sv.onload = function() {
+        sv.parentNode.removeChild(sv);
     };
 
-};
-
+    if (callback && typeof(callback) === "function") {
+        callback();
+    }
+}
